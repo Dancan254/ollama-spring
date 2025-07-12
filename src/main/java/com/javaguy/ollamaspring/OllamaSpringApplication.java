24cc7ce81eeb76a84aba691cb42168d44c97ef81
Dancan254 <dancanian25@gmail.com>
2025-07-12 23:48:56 +0300

    inititial project setup

--- src/main/java/com/javaguy/ollamaspring/OllamaSpringApplication.java
+++ src/main/java/com/javaguy/ollamaspring/OllamaSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OllamaSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OllamaSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaSpringApplication.class, args);
+    }
 
 }
