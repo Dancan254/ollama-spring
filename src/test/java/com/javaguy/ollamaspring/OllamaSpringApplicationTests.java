2609caa03e1083c7d4a6b8e4836e7803cae11041
Dancan254 <dancanian25@gmail.com>
2025-07-12 23:46:45 +0300

    Initial project setup

---
+++ src/test/java/com/javaguy/ollamaspring/OllamaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguy.ollamaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
