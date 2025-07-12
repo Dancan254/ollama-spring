9203236a59ea58f9ca0f2fc44b1f26667514c951
Dancan254 <dancanian25@gmail.com>
2025-07-12 23:48:24 +0300

    setup the Tika dependency to read pdf docs

--- src/test/java/com/javaguy/ollamaspring/OllamaSpringApplicationTests.java
+++ src/test/java/com/javaguy/ollamaspring/OllamaSpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OllamaSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
